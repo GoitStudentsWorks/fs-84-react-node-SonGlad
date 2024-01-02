64deabbb62194a87bb5d459523b605e3a056c5fe
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-01-02 23:03:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/HEALTH_EN">
+        <BrowserRouter basename="/fs-84-react-node-SonGlad">
           <ThemeProvider theme={theme}>
             <GlobalStyle />
             <App />
